945142099a4135deee47502c9a2db4240b7679ae
ThirteenAsh <1984885454@qq.com>
2026-01-24 16:34:13 +0800

    feat(dept): 为部门管理模块添加完整的日志记录功能
    - 在 .gitignore 文件中添加了必要的忽略规则配置
    - 在 DeptController 中引入了 SLF4J 日志框架并添加日志注解
    - 为所有部门操作接口添加详细的日志记录，包括查询、删除、新增、修改等功能
    - 创建了 logback.xml 配置文件实现控制台和文件双重日志输出
    - 配置了日志文件的滚动策略和历史保留机制
    - 更新了项目模块配置以排除日志目录的版本控制

--- web-management/src/main/java/com/thirteenash/controller/DeptController.java
+++ web-management/src/main/java/com/thirteenash/controller/DeptController.java
@@ -3,11 +3,13 @@ package com.thirteenash.controller;
 import com.thirteenash.pojo.Dept;
 import com.thirteenash.pojo.Result;
 import com.thirteenash.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j // 日志注解 日志记录
 @RestController//注解作用为：将当前类对象，注入到spring容器中，创建对象实例
 public class DeptController {
 
@@ -18,6 +20,7 @@ public class DeptController {
     //@RequestMapping(value = "/depts", method = RequestMethod.GET)
     @GetMapping("/depts")
     public Result list(){
+        log.info("查询全部的部门数据");
         List< Dept > deptList = deptService.findAll(); //封装 数据
         return Result.success(deptList);
     }
@@ -25,6 +28,7 @@ public class DeptController {
     // 删除部门
     @DeleteMapping("/depts")
     public Result delete(Integer id){
+        log.info("根据id删除部门：{}",id);
         deptService.deleteById(id);
         return Result.success();
     }
@@ -32,6 +36,7 @@ public class DeptController {
     //新增部门
     @PostMapping("/depts")
     public Result add(@RequestBody Dept dept){ //@RequestBody注解作用为：将前台发送的json数据，映射为Dept对象
+        log.info("新增部门: {}",dept);
         deptService.add(dept);
         return Result.success();
     }
@@ -39,6 +44,7 @@ public class DeptController {
     //根据ID查询部门
     @GetMapping("/depts/{id}")
     public Result getInfo(@PathVariable Integer id){ //@PathVariable注解作用为：将url中的参数，映射为方法参数 即：将url中的{id}参数，映射为id参数
+        log.info("根据ID查询数据:{}", id);
         Dept dept = deptService.getById(id);
         return Result.success(dept);
     }
@@ -46,6 +52,7 @@ public class DeptController {
     //修改 部门
     @PutMapping("/depts")
     public Result update(@RequestBody Dept dept){
+        log.info("修改部门：{}", dept);
         deptService.update(dept);
         return Result.success();
     }
